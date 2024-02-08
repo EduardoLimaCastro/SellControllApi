e78fb370a5c1285f13794cfdb53eb5fdf0a31109
Eduardo Castro <eduardo.alencar.castro@gmail.com>
2024-02-08 13:31:59 -0300

    First Commit

---
+++ sellcontrolapi/src/main/java/com/sellcontrol/sellcontrolapi/SellcontrolapiApplication.java
@@ -0,0 +1,13 @@
+package com.sellcontrol.sellcontrolapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SellcontrolapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SellcontrolapiApplication.class, args);
+	}
+
+}
---
+++ sellcontrolapi/src/test/java/com/sellcontrol/sellcontrolapi/SellcontrolapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sellcontrol.sellcontrolapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SellcontrolapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
